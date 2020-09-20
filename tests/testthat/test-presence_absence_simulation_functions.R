e67b7f91232b3482341b0457d1a5f86f9dbdfd73
Jordan Love <jordnlv@gmail.com>
2020-09-20 10:57:07 -0600

    Added first output check test for simulate presence absence data

--- tests/testthat/test-presence_absence_simulation_functions.R
+++ tests/testthat/test-presence_absence_simulation_functions.R
@@ -1,6 +1,6 @@
-context("Correlated Presence-Absence Simulation Function - Input Errors")
+context("Correlated Presence-Absence Simulation Function")
 
-test_that("Number of species is positive", {
+test_that("Inputs are conformable", {
   expect_error(
     simulate_presence_absence_data(S = 0),
     "The number of species, S, must be a positive integer."
@@ -16,4 +16,18 @@ test_that("Number of species is positive", {
     "The vector of latent mean presence must match the number of species, S."
     )
 
+  expect_error(
+    simulate_presence_absence_data(S = 3,
+                                   spp_presence_correlation = matrix(rep(0, 6), nrow = 2)),
+    "The presence correlation matrix must be square."
+  )
 })
+
+test_that("Outputs are Expected", {
+  expect_length(simulate_presence_absence_data(), 4)
+
+  ## TODO: Add Expected Rows in Dataframe
+  ## TODO: Add Expected Column Nmaes in Dataframe
+  ## TODO: Add Expected Values in Each Column of Dataframe
+})
+
