586ec727aebc0c7a1ef26cd1bfe35d0e9d915141
Jordan Love <jordnlv@gmail.com>
2020-09-20 10:23:45 -0600

    Added rlkjcorr function; Expanded testing for simulating presence_absence_simulation function

--- R/presence_absence_simulation_functions.R
+++ R/presence_absence_simulation_functions.R
@@ -1,3 +1,53 @@
+#' Sample LKJ correlation matrices.
+#'
+#' This function was copied from Richard McElreath's rethinking package hosted
+#' at https://github.com/rmcelreath/rethinking. In turn, he appears to have
+#' copied it from Ben Bolker's rLKJ function from the emdbook package, although
+#' I cannot find it there (else I would have imported it).
+#'
+#' @param n Number of matrices to sample.
+#' @param K dimenstion of matrix to sample.
+#' @param eta Distribution parameter
+#' @return matrix
+#'
+#' @importFrom stats rbeta rnorm
+#' @export
+rlkjcorr <- function (n, K, eta = 1) {
+
+  stopifnot(is.numeric(K), K >= 2, K == as.integer(K))
+  stopifnot(eta > 0)
+  #if (K == 1) return(matrix(1, 1, 1))
+
+  f <- function() {
+    alpha <- eta + (K - 2)/2
+    r12 <- 2 * rbeta(1, alpha, alpha) - 1
+    R <- matrix(0, K, K) # upper triangular Cholesky factor until return()
+    R[1,1] <- 1
+    R[1,2] <- r12
+    R[2,2] <- sqrt(1 - r12^2)
+    if(K > 2) for (m in 2:(K - 1)) {
+      alpha <- alpha - 0.5
+      y <- rbeta(1, m / 2, alpha)
+
+      # Draw uniformally on a hypersphere
+      z <- rnorm(m, 0, 1)
+      z <- z / sqrt(crossprod(z)[1])
+
+      R[1:m,m+1] <- sqrt(y) * z
+      R[m+1,m+1] <- sqrt(1 - y)
+    }
+    return(crossprod(R))
+  }
+  R <- replicate( n , f() )
+  if ( dim(R)[3]==1 ) {
+    R <- R[,,1]
+  } else {
+    # need to move 3rd dimension to front, so conforms to array structure that Stan uses
+    R <- aperm(R,c(3,1,2))
+  }
+  return(R)
+}
+
 #' Simulate Presence-Absence Data
 #'
 #' @param S The Number of Species
@@ -56,7 +106,7 @@ simulate_presence_absence_data <- function(S = 10,
   latent_mean_tibble <-
     rep(spp_mean_presence, N) %>%
     matrix(ncol = S, byrow = TRUE) %>%
-    dplyr::as_tibble(rownames = "plotid") %>%
+    dplyr::as_tibble(rownames = "plotid", .name_repair = "unique") %>%
     dplyr::mutate(plotid = as.numeric(plotid)) %>%
     tidyr::pivot_longer(cols = -contains("plotid"),
                  names_to = "species",
@@ -66,7 +116,7 @@ simulate_presence_absence_data <- function(S = 10,
   ### Prepare Latent Presence Realizations
   latent_presence_tibble <-
     latent_presence %>%
-    dplyr::as_tibble(rownames = "plotid") %>%
+    dplyr::as_tibble(rownames = "plotid", .name_repair = "unique") %>%
     dplyr::mutate(plotid = as.numeric(plotid)) %>%
     tidyr::pivot_longer(
       cols = -contains("plotid"),
@@ -78,7 +128,7 @@ simulate_presence_absence_data <- function(S = 10,
   ### Prepare True Presence Values
   presence_tibble <-
     presence_data %>%
-    dplyr::as_tibble(rownames = "plotid") %>%
+    dplyr::as_tibble(rownames = "plotid", .name_repair = "unique") %>%
     dplyr::mutate(plotid = as.numeric(plotid)) %>%
     tidyr::pivot_longer(
       cols = -contains("plotid"),
@@ -93,9 +143,9 @@ simulate_presence_absence_data <- function(S = 10,
   ### Prepare Observed Presence Values
   observation_tibble <-
     observation_data %>%
-    dplyr::as_tibble(rownames = "plotid") %>%
+    dplyr::as_tibble(rownames = "plotid", .name_repair = "unique") %>%
     dplyr::mutate(plotid = as.numeric(plotid)) %>%
-    dplyr::pivot_longer(
+    tidyr::pivot_longer(
       cols = -contains("plotid"),
       names_to = "species",
       values_to = "observed_presence"
--- tests/testthat/test-presence_absence_simulation_functions.R
+++ tests/testthat/test-presence_absence_simulation_functions.R
@@ -1,5 +1,19 @@
 context("Correlated Presence-Absence Simulation Function - Input Errors")
 
 test_that("Number of species is positive", {
-  expect_error(simulate_presence_absence_data(S = 0), "The number of species, S, must be a positive integer.")
+  expect_error(
+    simulate_presence_absence_data(S = 0),
+    "The number of species, S, must be a positive integer."
+    )
+
+  expect_error(
+    simulate_presence_absence_data(N = 0),
+    "The number of observations, N, must be a positive integer."
+    )
+
+  expect_error(
+    simulate_presence_absence_data(S = 3, spp_mean_presence = c(0.25, 0.5)),
+    "The vector of latent mean presence must match the number of species, S."
+    )
+
 })
