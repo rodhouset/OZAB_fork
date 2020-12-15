323e530ce1dad6b1038bbee5aa18b3a6e1dd4fbc
Jordan Love <jordnlv@gmail.com>
2020-12-14 23:03:38 -0700

    Added 'pivot_nps_data' function which takes data from the format often pulled from NPS datasets to a typical long format with covariates appropriately seperated into their own column

---
+++ R/data_functions.R
@@ -0,0 +1,22 @@
+#' NPS Data Format Transformation
+#'
+#' @param nps_data Tibble containing NPS Formatted Data
+#' @param covariate_cols Columns which refer to covariates such as Year, Location, Grade, etc.
+#'
+#' @return Long format tibble
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   pivot_nps_data(nps_data_ex, c(Year, Loc_Name, SCOSA))
+#' }
+pivot_nps_data <- function(nps_data, covariate_cols = NULL){
+  tidyr::pivot_longer(
+    data = nps_data,
+    cols = -c({{ covariate_cols }}),
+    names_to = 'Species',
+    values_to = 'Cover Class'
+    )
+}
+
+
