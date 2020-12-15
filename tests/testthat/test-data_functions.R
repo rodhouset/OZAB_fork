11b91224fb9512ee97851cf34dce171c63b67e8c
Jordan Love <jordnlv@gmail.com>
2020-12-14 23:12:47 -0700

    Added test functions for pivot_nps_data which handles covariates and non-covariate settings

---
+++ tests/testthat/test-data_functions.R
@@ -0,0 +1,40 @@
+test_that("NPS Data Transformation Function - No Covariates", {
+
+  test_tibble <-
+    dplyr::tibble(
+      Species1 = 1,
+      Species2 = 2,
+      Species3 = 3
+    )
+
+  expect_equal(
+    pivot_nps_data(test_tibble),
+    tidyr::pivot_longer(
+      test_tibble,
+      dplyr::everything(),
+      names_to = 'Species',
+      values_to = 'Cover Class')
+    )
+})
+
+test_that("NPS Data Transformation Function - Covariates", {
+
+  test_tibble <-
+    dplyr::tibble(
+      Year = 2020,
+      Location = 'Example Site',
+      Grade = 0.25,
+      Species1 = 1,
+      Species2 = 2,
+      Species3 = 3
+    )
+
+  expect_equal(
+    pivot_nps_data(test_tibble, covariate_cols = c(Year, Location, Grade)),
+    tidyr::pivot_longer(
+      test_tibble,
+      -c(Year, Location, Grade),
+      names_to = 'Species',
+      values_to = 'Cover Class')
+  )
+})
