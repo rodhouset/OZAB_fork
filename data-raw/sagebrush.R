7a3f65e4355dbe9c462ae1a949a899047a983dc0
Jordan Love <jordnlv@gmail.com>
2020-12-20 13:24:45 -0700

    Adjusted sagebrush data with species information

--- data-raw/sagebrush.R
+++ data-raw/sagebrush.R
@@ -4,11 +4,15 @@ sagebrush_raw <- readr::read_csv('data-raw/NPSDataFile_JABES_Irvineetal_OBHM.csv
 
 sagebrush <-
   sagebrush_raw %>%
+  filter(Type == 'Train') %>%
   select(BROTEC, ARTTRID, Fire, Topo, D_ROADS, Topo) %>%
   rename(
-    `Cover Class` = BROTEC,
+    `Bromus tectorum` = BROTEC,
+    `Artemisia tridentata` = ARTTRID,
     `Topography` = Topo,
     `Dist. to Road` = D_ROADS
-  )
+  ) %>%
+  tidyr::pivot_longer(cols = c(`Bromus tectorum`, `Artemisia tridentata`), names_to = 'Species', values_to = 'Cover Class') %>%
+  select(Species, `Cover Class`, Fire, Topography, `Dist. to Road`)
 
 usethis::use_data(sagebrush, overwrite = TRUE)
