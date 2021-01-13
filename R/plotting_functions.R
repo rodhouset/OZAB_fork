a17f4cd2debed927e7ee8607c7d726576698a91e
Jordan Love <jordnlv@gmail.com>
2021-01-13 16:41:19 -0700

    Added alluvial plot functionality and tests

--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -54,25 +54,56 @@ plot_cover_class_by_time_and_location <- function(data, species, datetime_col, l
     ggplot2::geom_tile()
 }
 
-alluvial_plot <- function(.data, covariate, cover_class_col = .data$`Cover Class`){
-  ## Known Bugs:
-  ##  1. cover class column name needs to be generalizable
-  ##  2. find a replacement for rlang::as_stirng(rlang::enym(.))
-  ##  3. generalize function to arbitrarily many variables
-  ##  4. add variables for widths
-  ##  5. add variables for y-axis name (or remove custom naming)
-  ##  6. add stratum names
+# alluvial_plot <- function(.data, covariates, cover_class_col = .data$`Cover Class`, alluvium_width = 1/12, stratum_width = 1/8){
+#   ## Known Bugs:
+#   ##  1. cover class column name needs to be generalizable
+#   ##  2. find a replacement for rlang::as_stirng(rlang::enym(.))
+#   ##  3. generalize function to arbitrarily many variables
+#
+#   ## Add Tests For:
+#   ## 1. covariate not provided
+#   ## 2. covariate not found
+#   ## 3. covariate is not discrete
+#   ## 4. Errors when widths are incorrect
+#
+#   .data %>%
+#     dplyr::group_by({{ cover_class_col }}, {{ covariate }}) %>%
+#     dplyr::tally() %>%
+#     ggplot2::ggplot(ggplot2::aes(y = n, axis1 = {{ cover_class_col }}, axis2 = {{ covariate }}, axis3 = NULL, axis4 = NULL)) +
+#     ggalluvial::geom_alluvium(ggplot2::aes(fill = {{ cover_class_col }}), width = alluvium_width) +
+#     ggalluvial::geom_stratum(width = stratum_width) +
+#     ggplot2::geom_label(stat = "stratum", ggplot2::aes(label = after_stat(stratum))) +
+#     ggplot2::scale_x_discrete(limits = c('Cover Class', rlang::as_name(covariate)), expand = c(.05, .05))
+# }
 
-  ## Add Tests For:
-  ## 1. covariate not provided
-  ## 2. covariate not found
-  ## 3. covariate is not discrete
+alluvial_plot <- function(.data, ..., cover_class_col = .data$`Cover Class`, alluvium_width = 1/12, stratum_width = 1/8){
+
+  covariates <- rlang::enquos(...)
+
+  non_null_args <- length(covariates)
+
+  # Check if covariates were provided
+  if(length(non_null_args) == 0){
+    stop('No Covariates Provided')
+  }
+
+  # Check if columns are found in the df
+  for(col in covariates){
+    if(!(rlang::quo_name(col) %in% names(.data))){
+      stop(glue::glue('{ rlang::quo_name(col) } not found in provided data'))
+    }
+  }
+
+  for(i in (length(covariates) + 1):4){
+    covariates[[i]] <- quo(NULL)
+  }
 
   .data %>%
-    dplyr::group_by({{ cover_class_col }}, {{ covariate }}) %>%
-    dplyr::tally(name = 'Freq') %>%
-    ggplot2::ggplot(ggplot2::aes(y = Freq, axis1 = {{ cover_class_col }}, axis2 = {{ covariate }})) +
-    ggalluvial::geom_alluvium(ggplot2::aes(fill = {{ cover_class_col }}), width = 1/12) +
-    ggalluvial::geom_stratum(width = 1/8) +
-    ggplot2::scale_x_discrete(limits = c('Cover Class', rlang::as_string(rlang::ensym(covariate))), expand = c(.05, .05))
+    dplyr::group_by(!!! covariates) %>%
+    dplyr::tally() %>%
+    ggplot2::ggplot(ggplot2::aes(y = n, axis1 = !! covariates[[1]], axis2 = !! covariates[[2]], axis3 = !! covariates[[3]], axis4 = !! covariates[[4]] )) +
+      ggalluvial::geom_alluvium(ggplot2::aes(fill = !! covariates[[1]]), width = alluvium_width) +
+      ggalluvial::geom_stratum(width = stratum_width) +
+      ggplot2::geom_label(stat = "stratum", ggplot2::aes(label = after_stat(stratum))) +
+      ggplot2::scale_x_discrete(limits = sapply(covariates[1:non_null_args], rlang::quo_name), expand = c(.05, .05))
 }
--- tests/testthat/test-plotting_functions.R
+++ tests/testthat/test-plotting_functions.R
@@ -46,3 +46,21 @@ test_that("Plot Cover Class By Time Works", {
     plot_cover_class_by_time_and_location(test_tibble, 'A', Datetime, Location)
   )
 })
+
+test_that("Alluvial Plot Function Works", {
+  test_tibble <-
+    tibble(Species = forcats::as_factor(c('A', 'A', 'B', 'B')),
+           `Cover Class` = forcats::as_factor(c(0, 1, 2, 2)),
+           Categorical = forcats::as_factor(c(1, 0, 1, 1))
+           )
+
+  ## Error in no covariates provided
+  expect_error(
+    alluvial_plot(test_tibble)
+  )
+
+  ## Error when column not found
+  expect_error(
+    alluvial_plot(test_tibble, `Missing Column`)
+  )
+})
